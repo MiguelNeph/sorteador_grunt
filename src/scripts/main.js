6af0119692853e5dc7792b6a7b111055dc62451b
Miguel Sobrino <miguel@sobrino.es>
2023-08-28 12:31:43 +0100

    initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('form-sort').addEventListener('submit', function(evento){
+        evento.preventDefault();
+        let maxNumber = document.getElementById('max-number').value;
+        maxNumber = parseInt(maxNumber);
+
+        let randomNumber = Math.random() * maxNumber;
+        randomNumber = Math.floor(randomNumber + 1);
+
+        document.getElementById('result-value').innerText=randomNumber;
+        document.querySelector('.result').style.display = 'block';
+    })
+})
